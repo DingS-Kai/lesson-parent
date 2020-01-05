82d32e643af32e2ad7a70d8950f2a9d075cc65f8
Lihengzhen <592267445@qq.com>
2020-01-05 17:00:33 +0800

    重新逆向生成preschedule表

---
+++ userManagerImpl/src/main/com/fosu/lesson/dao/TPrescheduleMapper.java
@@ -0,0 +1,22 @@
+package com.fosu.lesson.dao;
+
+import com.fosu.lesson.pojo.TPreschedule;
+import com.fosu.lesson.pojo.TPrescheduleExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface TPrescheduleMapper {
+    long countByExample(TPrescheduleExample example);
+
+    int deleteByExample(TPrescheduleExample example);
+
+    int insert(TPreschedule record);
+
+    int insertSelective(TPreschedule record);
+
+    List<TPreschedule> selectByExample(TPrescheduleExample example);
+
+    int updateByExampleSelective(@Param("record") TPreschedule record, @Param("example") TPrescheduleExample example);
+
+    int updateByExample(@Param("record") TPreschedule record, @Param("example") TPrescheduleExample example);
+}
---
+++ userManagerImpl/src/main/com/fosu/lesson/pojo/TPreschedule.java
@@ -0,0 +1,143 @@
+package com.fosu.lesson.pojo;
+
+public class TPreschedule {
+    private Integer id;
+
+    private String scheduleId;
+
+    private String classId;
+
+    private String classroomId;
+
+    private String courseId;
+
+    private String teacherId;
+
+    private String timeId;
+
+    private String remark;
+
+    private String className;
+
+    private String place;
+
+    private String courseName;
+
+    private String teacherName;
+
+    private String day;
+
+    private String hour;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getScheduleId() {
+        return scheduleId;
+    }
+
+    public void setScheduleId(String scheduleId) {
+        this.scheduleId = scheduleId == null ? null : scheduleId.trim();
+    }
+
+    public String getClassId() {
+        return classId;
+    }
+
+    public void setClassId(String classId) {
+        this.classId = classId == null ? null : classId.trim();
+    }
+
+    public String getClassroomId() {
+        return classroomId;
+    }
+
+    public void setClassroomId(String classroomId) {
+        this.classroomId = classroomId == null ? null : classroomId.trim();
+    }
+
+    public String getCourseId() {
+        return courseId;
+    }
+
+    public void setCourseId(String courseId) {
+        this.courseId = courseId == null ? null : courseId.trim();
+    }
+
+    public String getTeacherId() {
+        return teacherId;
+    }
+
+    public void setTeacherId(String teacherId) {
+        this.teacherId = teacherId == null ? null : teacherId.trim();
+    }
+
+    public String getTimeId() {
+        return timeId;
+    }
+
+    public void setTimeId(String timeId) {
+        this.timeId = timeId == null ? null : timeId.trim();
+    }
+
+    public String getRemark() {
+        return remark;
+    }
+
+    public void setRemark(String remark) {
+        this.remark = remark == null ? null : remark.trim();
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className == null ? null : className.trim();
+    }
+
+    public String getPlace() {
+        return place;
+    }
+
+    public void setPlace(String place) {
+        this.place = place == null ? null : place.trim();
+    }
+
+    public String getCourseName() {
+        return courseName;
+    }
+
+    public void setCourseName(String courseName) {
+        this.courseName = courseName == null ? null : courseName.trim();
+    }
+
+    public String getTeacherName() {
+        return teacherName;
+    }
+
+    public void setTeacherName(String teacherName) {
+        this.teacherName = teacherName == null ? null : teacherName.trim();
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day == null ? null : day.trim();
+    }
+
+    public String getHour() {
+        return hour;
+    }
+
+    public void setHour(String hour) {
+        this.hour = hour == null ? null : hour.trim();
+    }
+}
